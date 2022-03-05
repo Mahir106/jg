0270d25a4e6993c3670f6338b11995b993293d37
Mahir106 <83213311+Mahir106@users.noreply.github.com>
2022-03-05 21:42:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+leftWristX = "";
+rightWristX = "";
+difference = "";
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Posenet is initalized");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+
+     console.log(results);
+     
+     leftWristX = results[0].pose.leftWrist.x;
+     rightWristX = results[0].pose.rightWrist.x;
+     difference = floor(leftWristX - rightWristX);
+     console.log("leftWristx: "+leftWristX+"rightWristx: "+ rightWristX+"difference: "+difference);
+    }
+}
+
+function draw(){
+background("light blue")
+
+textSize(difference);
+fill("Red");
+text('Mahir', 20, 400);
+}
